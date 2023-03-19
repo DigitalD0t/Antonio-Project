c6a47a0696c1fe6528bdb66361e36810f2c3f8fd
loginSuccess <37210312+Brandon-Moreno@users.noreply.github.com>
2023-03-18 19:34:52 -0600

    desktop
    haciendo la vista de escritorios y laptop

--- js/app.js
+++ js/app.js
@@ -2,4 +2,4 @@ const cerrar = document.querySelector(".aceptar");
 cerrar.addEventListener("click", function(){
   const aviso = document.querySelector(".aviso");
   aviso.style.display="none";
-})
+});
