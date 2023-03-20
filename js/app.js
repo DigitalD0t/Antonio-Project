4e897983a42f7dc8c2cade84ded918ea6806a363
brandon-moreno <brandonrasta83@gmail.com>
2023-03-20 04:01:17 -0600

    i can say that i finish it, almost

--- js/app.js
+++ js/app.js
@@ -3,3 +3,12 @@ cerrar.addEventListener("click", function(){
   const aviso = document.querySelector(".aviso");
   aviso.style.display="none";
 });
+
+document.addEventListener('DOMContentLoaded',()=>{
+  const imgLightBox = document.querySelectorAll('.evidencia');
+  M.Materialbox.init(imgLightBox); 
+  const elems = document.querySelectorAll('.collapsible');
+  const instaces = M.Collapsible.init(elems,{
+    accordion:false
+  });
+});
